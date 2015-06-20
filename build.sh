3b0fac271fc3cff7906efe65f21f140dfdb1f740
Christopher Small <metasoarous@gmail.com>
2015-06-19 21:58:37 -0700

    Rewrote build.sh with env variables

--- build.sh
+++ build.sh
@@ -3,15 +3,26 @@
 # Sane error handling settings
 set -euf -o pipefail
 
+
+# Specify output directory, and name input data
+outdir="output"
+metadata="data/sfv.csv"
+
+
 # Compute number of sequences per species
-csvuniq -zc species data/sfv.csv > output/seqs_per_species.csv
+seqs_per_species="$outdir/seqs_per_species.csv"
+csvuniq -zc species $metadata > $seqs_per_species
 
 # Compute number of sequences per specimen
-csvuniq -zc specimen,species,location data/sfv.csv > output/seqs_per_specimen.csv
+seqs_per_specimen="$outdir/seqs_per_specimen.csv"
+csvuniq -zc specimen,species,location $metadata > $seqs_per_specimen
 
-# Use those results to count specimens per species
-csvuniq -zc species output/seqs_per_specimen.csv > output/specs_per_species.csv
+# Use those results to compute number of specimens per species
+specs_per_species="$outdir/specs_per_species.csv"
+csvuniq -zc species $seqs_per_specimen > $specs_per_species
 
-# Also use them to count specimens by species and location
-csvuniq -zc species,location output/seqs_per_specimen.csv > output/specs_per_species_location.csv
+# Also use them to compute number of specimens per species and location
+specs_per_species_location="$outdir/specs_per_species_location.csv"
+csvuniq -zc species,location $seqs_per_specimen > $specs_per_species_location
+
 
