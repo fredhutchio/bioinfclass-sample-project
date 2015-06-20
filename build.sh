a4b889323d7c27282904cb441e70234648bf4a30
Christopher Small <metasoarous@gmail.com>
2015-06-19 22:18:37 -0700

    Added previous tree and alignment analysis to build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,11 @@ set -euf -o pipefail
 # Specify output directory, and name input data
 outdir="output"
 metadata="data/sfv.csv"
+inseqs="data/sfv.fasta"
 
+
+# Basic metadata stats
+# --------------------
 
 # Compute number of sequences per species
 seqs_per_species="$outdir/seqs_per_species.csv"
@@ -26,3 +30,13 @@ specs_per_species_location="$outdir/specs_per_species_location.csv"
 csvuniq -zc species,location $seqs_per_specimen > $specs_per_species_location
 
 
+# Sequence analysis
+# -----------------
+
+# Alignment
+alignment="$outdir/alignment.fasta"
+muscle -maxiters 2 -in $inseqs -out $alignment
+
+# Phylognetic tree
+tree="$outdir/tree.nw"
+FastTree -nt $alignment > $tree
