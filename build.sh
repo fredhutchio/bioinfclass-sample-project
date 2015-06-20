2c1779995b7c4b5aec404af4c12b2ac2f2d9b6cc
Christopher Small <metasoarous@gmail.com>
2015-06-19 21:56:25 -0700

    Added other metadata counting steps

--- build.sh
+++ build.sh
@@ -6,3 +6,12 @@ set -euf -o pipefail
 # Compute number of sequences per species
 csvuniq -zc species data/sfv.csv > output/seqs_per_species.csv
 
+# Compute number of sequences per specimen
+csvuniq -zc specimen,species,location data/sfv.csv > output/seqs_per_specimen.csv
+
+# Use those results to count specimens per species
+csvuniq -zc species output/seqs_per_specimen.csv > output/specs_per_species.csv
+
+# Also use them to count specimens by species and location
+csvuniq -zc species,location output/seqs_per_specimen.csv > output/specs_per_species_location.csv
+
